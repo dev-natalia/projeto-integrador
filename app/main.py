988b99169ceb1fcb67aa7d813bf9ab7b19a1bd37
Natalia <dev.nataliaps@gmail.com>
2026-04-19 14:09:31 -0300

    criação de arquivo main.py

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app.database import Base, engine
+from app.routes.bairro_routes import router as bairro_router
+from app.models import bairro, curso, atividade
+
+app = FastAPI(title="Projeto Integrador API", version="1.0")
+
+app.include_router(bairro_router)
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/", response_model=dict)
+def root() -> dict[str, str]:
+    """Retorna um status simples da API."""
+    return {"msg": "API funcionando"}
